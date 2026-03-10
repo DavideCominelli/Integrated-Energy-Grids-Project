71ea3bd1d7c8eb9c40a05dc73e245532d123b8b6
mariajosseu <s252605@dtu.dk>
2026-03-10 21:34:56 +0100

    last updates

--- main.py
+++ main.py
@@ -2,69 +2,99 @@
 import pandas as pd
 import pypsa
 import logging
-from data.data import load_wind, preprocessing_consumption_data, load_raw_consumption_data
-from plots import plot_demand
-#%%
-# Configure logging
-logging.basicConfig(level=logging.INFO)
 
-#%%
-# input data for the network
-logging.info("Loading consumptiondata...")
-df_demand_raw = load_raw_consumption_data()
-plot_demand(df_demand_raw)
-df_demand = preprocessing_consumption_data(df_demand_raw)
-plot_demand(df_demand)
-
-logging.info("Loading wind data...")
-wind_data = load_wind()
-
-# %%
-logging.info("Inicialize empty network...")
+#%% create a new PyPSA network
 network = pypsa.Network()
 
-# add snapshots for the whole year of 2024 in hourly resolution
-hours_in_2024 = pd.date_range('2024-01-01 00:00Z',
-                              '2024-12-31 23:00Z',
+# add snapshots for the whole year of 2015 in hourly resolution
+hours_in_2015 = pd.date_range('2015-01-01 00:00Z',
+                              '2015-12-31 23:00Z',
                               freq='h')
 
-network.set_snapshots(hours_in_2024.values)
+network.set_snapshots(hours_in_2015.values)
 
 # copper plate model (1 bus bar)
 network.add("Bus",
-            "electricity")
+            "electricity bus")
+
+
+#%% add demand data
+# load electricity demand data
+df_elec = pd.read_csv('data/electricity_demand.csv', sep=';', index_col=0) # in MWh
+df_elec.index = pd.to_datetime(df_elec.index) #change index to datatime
+country= 'DEU' # Germany
 
 # add load to the bus
 network.add("Load",
             "load",
-            bus="electricity",
-            p_set=df_demand['ConsumptionMWh'].values)
+            bus="electricity bus",
+            p_set=df_elec[country].values)
 
+
+#%% create annuity function
+def annuity(n,r):
+    """ Calculate the annuity factor for an asset with lifetime n years and
+    discount rate  r """
+
+    if r > 0:
+        return r/(1. - 1./(1.+r)**n)
+    else:
+        return 1/n
+
+#%% include solar, wind and gas generators
+network.add("Carrier", "gas", co2_emissions=0.19) # in t_CO2/MWh_th
+network.add("Carrier", "onshorewind")
+network.add("Carrier", "solar")
+
+# add onshore wind generator
+df_onshorewind = pd.read_csv('data/onshore_wind_1979-2017.csv', sep=';', index_col=0)
+df_onshorewind.index = pd.to_datetime(df_onshorewind.index)
+CF_wind = df_onshorewind[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
+capital_cost_onshorewind = annuity(30,0.07)*910000*(1+0.033) # in €/MW
 
 # %%
-carriers = [
-    "onwind",
-    "solar",
-    "OCGT", #open cycle gas turbine
-    "CCGT", #combined cycle gas turbine
-    "battery storage",
-]
-
-network.add(
-    "Carrier",
-    carriers,
-    color=["dodgerblue", "gold", "indianred","yellow-green", "brown"],
-)
+network.add("Generator",
+            "onshorewind",
+            bus="electricity bus",
+            p_nom_extendable=True,
+            carrier="onshorewind",
+            #p_nom_max=1000, # maximum capacity can be limited due to environmental constraints
+            capital_cost = capital_cost_onshorewind,
+            marginal_cost = 0,
+            p_max_pu = CF_wind.values)
 # %%
-logging.info("Add demanda into the network...")
-network.add("Load",
-      "demand",
-      bus="electricity",
-      p_set=df_demand['ConsumptionMWh'].values)
+df_solar = pd.read_csv('data/pv_optimal.csv', sep=';', index_col=0)
+df_solar.index = pd.to_datetime(df_solar.index)
+CF_solar = df_solar[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
+capital_cost_solar = annuity(25,0.07)*425000*(1+0.03) # in €/MW
+network.add("Generator",
+            "solar",
+            bus="electricity bus",
+            p_nom_extendable=True,
+            carrier="solar",
+            #p_nom_max=1000, # maximum capacity can be limited due to environmental constraints
+            capital_cost = capital_cost_solar,
+            marginal_cost = 0,
+            p_max_pu = CF_solar.values)
+# %%
+capital_cost_OCGT = annuity(25,0.07)*560000*(1+0.033) # in €/MW
+fuel_cost = 21.6 # in €/MWh_th
+efficiency = 0.39 # MWh_elec/MWh_th
+marginal_cost_OCGT = fuel_cost/efficiency # in €/MWh_el
+network.add("Generator",
+            "OCGT",
+            bus="electricity bus",
+            p_nom_extendable=True,
+            carrier="gas",
+            #p_nom_max=1000,
+            capital_cost = capital_cost_OCGT,
+            marginal_cost = marginal_cost_OCGT)
 
-#%%
-year = 2030
-url = f"https://raw.githubusercontent.com/PyPSA/technology-data/v0.11.0/outputs/costs_{year}.csv"
-costs = pd.read_csv(url, index_col=[0, 1])
+# %%
+network.optimize(solver_name='gurobi')
 
+# Save the model to LP format
+network.model.to_file('model_A.lp')
+print(f"Model saved to: model_A.lp")
 
+# %%
